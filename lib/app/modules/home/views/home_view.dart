b2b638d8daae6248d0f6ff1522d62f13390e7858
nezam10 <nezam@bdtask.net>
2023-02-12 19:49:06 +0600

    add willpopscope method

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 import '../../../global_widgets/custom_drawer.dart';
@@ -9,51 +12,84 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      key: controller.globalKey,
-      drawer: const Drawer(
-        //backgroundColor: Colors.white,
-        child: CustomDrawer(),
+    return WillPopScope(
+      onWillPop: () {
+        return openDialog();
+      },
+      child: Scaffold(
+        extendBody: true,
+        key: controller.globalKey,
+        drawer: const Drawer(
+          //backgroundColor: Colors.white,
+          child: CustomDrawer(),
+        ),
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () {
+                controller.globalKey.currentState?.openDrawer();
+                print("openDrawer");
+              },
+              icon: const Icon(Icons.menu)),
+        ),
+        body: GetBuilder<HomeController>(
+          //initState: (_) {},
+          builder: (_) {
+            return Center(
+              child: _.navigationScreen.elementAt(_.selectedIndex),
+            );
+          },
+        ),
+        bottomNavigationBar: GetBuilder<HomeController>(
+          //initState: (_) {},
+          builder: (controller) {
+            return TabBarMaterialWidget(
+              selectIndex: controller.selectedIndex,
+              onChangedTab: (value) async {
+                controller.onItemTapped(value);
+                print("value == $value");
+              },
+            );
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(90)),
+          onPressed: () {
+            controller.incrementTab(1);
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
       ),
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-        leading: IconButton(
+    );
+  }
+
+  /* ::::::::::::::::::::::: OnWillPop show dialog :::::::::::::::::::::::*/
+  openDialog() {
+    Get.dialog(
+      AlertDialog(
+        title: const Text('Are Yor Sure?'),
+        content: const Text('Do you want to exit this App'),
+        actions: [
+          TextButton(
+            onPressed: () => Get.back(),
+            child: const Text("No"),
+          ),
+          TextButton(
             onPressed: () {
-              controller.globalKey.currentState?.openDrawer();
-              print("openDrawer");
+              if (Platform.isAndroid) {
+                SystemNavigator.pop();
+                print("android>>>>");
+              } else if (Platform.isIOS) {
+                exit(0);
+              }
             },
-            icon: const Icon(Icons.menu)),
-      ),
-      body: GetBuilder<HomeController>(
-        //initState: (_) {},
-        builder: (_) {
-          return Center(
-            child: _.navigationScreen.elementAt(_.selectedIndex),
-          );
-        },
-      ),
-      bottomNavigationBar: GetBuilder<HomeController>(
-        //initState: (_) {},
-        builder: (controller) {
-          return TabBarMaterialWidget(
-            selectIndex: controller.selectedIndex,
-            onChangedTab: (value) async {
-              controller.onItemTapped(value);
-              print("value == $value");
-            },
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(90)),
-        onPressed: () {
-          controller.incrementTab(1);
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+            child: const Text("Yes"),
+          ),
+        ],
       ),
     );
   }
